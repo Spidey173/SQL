97bfbb0f24d07904cbf121c7484f3e7c41142789
Pruthvi R <pruthvir6666@gmail.com>
2026-08-20 13:39:32 +0530

    Add more employees and separate Manager table in Q3

--- Employees_earning_more_than_manager.sql
+++ Employees_earning_more_than_manager.sql
@@ -2,6 +2,12 @@
 Question 3: Retrieve employees who earn more than their manager.
 */
 
+CREATE TABLE Manager (
+    manager_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    salary INT
+);
+
 CREATE TABLE Employee (
     id INT PRIMARY KEY,
     name VARCHAR(50),
@@ -9,13 +15,20 @@ CREATE TABLE Employee (
     manager_id INT
 );
 
+INSERT INTO Manager (manager_id, name, salary) VALUES
+(101, 'Sam', 60000),
+(102, 'Max', 90000),
+(103, 'Sarah', 75000);
+
 INSERT INTO Employee (id, name, salary, manager_id) VALUES
-(1, 'Joe', 70000, 3),
-(2, 'Henry', 80000, 4),
-(3, 'Sam', 60000, NULL),
-(4, 'Max', 90000, NULL);
+(1, 'Joe', 70000, 101),
+(2, 'Henry', 80000, 102),
+(3, 'Alex', 50000, 101),
+(4, 'Rachel', 85000, 103),
+(5, 'Daniel', 65000, 102),
+(6, 'Emma', 90000, 103);
 
 SELECT e.name AS Employee
 FROM Employee e
-JOIN Employee m ON e.manager_id = m.id
+JOIN Manager m ON e.manager_id = m.manager_id
 WHERE e.salary > m.salary;
